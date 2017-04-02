3b75ac430b7c9dafe9c3ed2509fa64ecf36c9161
Shachar Ohana <shachar.o@gmail.com>
2017-04-02 21:27:25 +0300

    WIP

--- tracy.c
+++ tracy.c
@@ -35,66 +35,26 @@ typedef struct {
 } msg_data_t;
 
 
-static __thread error_stack_t local_stack;
-static __thread int stack_ptr = 0;
-static __thread int stack_ptr_save = 0;
-static __thread msg_data_t msg_buf;
+/* ----------------------------- Static Data ------------------------------ */
 
 
 /* Get the string error corresponding to the numeric value of `err`. */
 static char const * get_error_string(int err);
 
 
-/* Get the string error corresponding to the numeric value of `err`. */
-static char const * get_error_string(int err) {
-  static __thread char errbuf[MAX_ERR_STR_BUF_SIZE + 1];
-
-  #if ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE)
-  if (strerror_r(err, errbuf, MAX_ERR_STR_BUF_SIZE) != 0) {
-    fprintf(stderr, "Error: strerror_r returned errno %d\n", errno);
-    return NULL;
-  }
-  return errbuf;
-#else
-  return strerror_r(err, errbuf, MAX_ERR_STR_BUF_SIZE);
-#endif
-
-}
+static __thread error_stack_t local_stack;
+static __thread int stack_ptr = 0;
+static __thread int stack_ptr_save = 0;
+static __thread msg_data_t msg_buf;
 
 
-/* Start an error traceback. */
-void start_error(char const * file, char const * func, int line) {
-  add_error_trace(file, func, line);
-}
-
-
-/* Save the initial error message. */
-void set_error_msg(char const * fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  vsnprintf(msg_buf.buff, MAX_USER_ERR_MSG_SIZE, fmt, args);
-  va_end(args);
-}
+/* --------------------------- Public Functions --------------------------- */
 
 
 /* Clear the error traceback. */
 void clear_error(void) {
   stack_ptr = 0;
   msg_buf.buff[0] = '\0';
-}
-
-
-/* Add a trace point to the error traceback. */
-void add_error_trace(char const * file, char const * func, int line) {
-  if (stack_ptr >= MAX_STACK_SIZE) {
-    fprintf(stderr, "Warning: error stack overflow (no room for stack trace)\n");
-    return;
-  }
-
-  local_stack.buffer[stack_ptr].file = file;
-  local_stack.buffer[stack_ptr].func = func;
-  local_stack.buffer[stack_ptr].line = line;
-  ++stack_ptr;
 }
 
 
@@ -141,6 +101,54 @@ void log_and_clear_on_error(err_t err) {
   if (err != OK) {
     log_and_clear_error(err);
   }
+}
+
+
+/* -------------------------- Private Functions --------------------------- */
+
+
+/* Start an error traceback. */
+void __start_error(char const * file, char const * func, int line) {
+  __add_error_trace(file, func, line);
+}
+
+
+/* Add a trace point to the error traceback. */
+void __add_error_trace(char const * file, char const * func, int line) {
+  if (stack_ptr >= MAX_STACK_SIZE) {
+    fprintf(stderr, "Warning: error stack overflow (no room for stack trace)\n");
+    return;
+  }
+
+  local_stack.buffer[stack_ptr].file = file;
+  local_stack.buffer[stack_ptr].func = func;
+  local_stack.buffer[stack_ptr].line = line;
+  ++stack_ptr;
+}
+
+
+/* Save the initial error message. */
+void __set_error_msg(char const * fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(msg_buf.buff, MAX_USER_ERR_MSG_SIZE, fmt, args);
+  va_end(args);
+}
+
+
+/* Get the string error corresponding to the numeric value of `err`. */
+static char const * get_error_string(int err) {
+  static __thread char errbuf[MAX_ERR_STR_BUF_SIZE + 1];
+
+  #if ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE)
+  if (strerror_r(err, errbuf, MAX_ERR_STR_BUF_SIZE) != 0) {
+    fprintf(stderr, "Error: strerror_r returned errno %d\n", errno);
+    return NULL;
+  }
+  return errbuf;
+#else
+  return strerror_r(err, errbuf, MAX_ERR_STR_BUF_SIZE);
+#endif
 }
 
 
--- tracy.h
+++ tracy.h
@@ -11,7 +11,7 @@ enum { OK = 0 };
 
 #ifdef __cplusplus
 #define __TRACY_NAMESPACE_BEGIN namespace tracy {
-#define __TRACY_NAMESPACE_END }  /* namespace tracy */
+#define __TRACY_NAMESPACE_END }
 #define __TRACY_SCOPE tracy::
 #else
 #define __TRACY_NAMESPACE_BEGIN
@@ -23,21 +23,7 @@ enum { OK = 0 };
 __TRACY_NAMESPACE_BEGIN
 
 
-/* Start an error traceback. */
-void start_error(char const * file, char const * func, int line);
-
-
-/* Add a trace point to the error traceback. */
-void add_error_trace(char const * file, char const * func, int line);
-
-
-/* Save the initial error message. */
-void set_error_msg(char const * fmt, ...);
-#ifdef __cplusplus
-/* Overload `set_error_msg` to allow empty __VA_ARGS__ in macros */
-/* in C++. */
-inline void set_error_msg(void) {}
-#endif 
+/* --------------------------- Public Functions --------------------------- */
 
 
 /* Clear the error traceback. */
@@ -64,14 +50,34 @@ void log_and_clear_error(err_t err);
 void log_and_clear_on_error(err_t err);
 
 
+/* -------------------------- Private Functions --------------------------- */
+
+
+/* Start an error traceback. */
+void __start_error(char const * file, char const * func, int line);
+
+
+/* Add a trace point to the error traceback. */
+void __add_error_trace(char const * file, char const * func, int line);
+
+
+/* Save the initial error message. */
+void __set_error_msg(char const * fmt, ...);
+#ifdef __cplusplus
+/* Overload `__set_error_msg` to allow empty __VA_ARGS__ in macros */
+/* in C++. */
+inline void __set_error_msg(void) {}
+#endif 
+
+
 /* -------------------------- Start Error Macros -------------------------- */
 
 
 /* Start an error traceback and return the error number. */
 /* If an error message is given, sets the error message to the traceback. */
 #define START_ERROR(err, ...) do { \
-  __TRACY_SCOPE start_error(__FILE__, __FUNCTION__, __LINE__); \
-  __TRACY_SCOPE set_error_msg(__VA_ARGS__); \
+  __TRACY_SCOPE __start_error(__FILE__, __FUNCTION__, __LINE__); \
+  __TRACY_SCOPE __set_error_msg(__VA_ARGS__); \
   return (err); \
 } while (0);
 
@@ -115,7 +121,7 @@ void log_and_clear_on_error(err_t err);
 
 /* Add current position to traceback and return `err`. */
 #define RETURN_ERROR(err) do { \
-  __TRACY_SCOPE add_error_trace(__FILE__, __FUNCTION__, __LINE__); \
+  __TRACY_SCOPE __add_error_trace(__FILE__, __FUNCTION__, __LINE__); \
   return (err); \
 } while (0)
 
