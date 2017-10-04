c1d5245e2eea0615b5f3fb7f68672212eb02a19c
Shachar Ohana <shachar.o@gmail.com>
2017-10-04 22:10:36 +0300

    Plz  god

--- src/tracy.c
+++ src/tracy.c
@@ -2,16 +2,16 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+
+#define __STDC_WANT_LIB_EXT1__ 1
 #include <string.h>
 
 
-#if defined(_WIN32) || defined(_WIN64)
-#include <windows.h>
-#include <winbase.h>
+/*#if defined(_WIN32) || defined(_WIN64)
 #define TRC_str_error(errno, buffer, size) strerror_s((buffer), (size), (errno))
-#else
+#else*/
 #define TRC_str_error(errno, buffer, size) strerror_r((errno), (buffer), (size))
-#endif
+
 
 
 enum { 
